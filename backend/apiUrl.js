be4c5c4cc0e42f085e27c77bd9d5f1c996255e29
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-11-22 15:14:34 -0300

    agrego backend 7

--- backend/apiUrl.js
+++ backend/apiUrl.js
@@ -1,3 +1,6 @@
 //backend/apiUrl.js
 
-export const API_URL = import.meta.env.VITE_API_URL;
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://create-tasks-pi.vercel.app"
+    : "http://localhost:4000";

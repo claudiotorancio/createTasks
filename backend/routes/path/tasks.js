59d75e488803c9765d0134e7ca5079ea3748e315
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-11-25 21:53:50 -0300

    configuro rutas de react dom en router.js 2

---
+++ backend/routes/path/tasks.js
@@ -0,0 +1,19 @@
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Asegúrate de que esta ruta apunta correctamente al directorio 'public'
+const output = path.resolve(__dirname, "../../../src/components");
+
+const tasks = async (req, res) => {
+  try {
+    res.sendFile(path.join(output, "TaskList"));
+  } catch (error) {
+    console.error("Error al enviar el archivo:", error.message);
+    res.status(500).send({ error: "Error al enviar el archivo." });
+  }
+};
+
+export default tasks;
